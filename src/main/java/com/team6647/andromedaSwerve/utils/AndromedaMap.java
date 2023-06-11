2729e3122e4f6476c68b16ffcf475e2e24fe187e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-11 10:18:15 -0600

    Created andromedaMap

---
+++ src/main/java/com/team6647/andromedaSwerve/utils/AndromedaMap.java
@@ -0,0 +1,45 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+
+package com.team6647.andromedaSwerve.utils;
+
+/**
+ * References the IDs of the motors & PID constants
+ */
+public class AndromedaMap {
+
+    /* IDs are set from left to right, and from top to bottom */
+    public static final int driveID1 = 1;
+    public static final int steerID1 = 2;
+    public static final int absEncoder1 = 3;
+
+    public static final double turning1P = 0.0;
+    public static final double turning1I = 0.0;
+    public static final double turning1D = 0.0;
+
+    public static final int driveID2 = 4;
+    public static final int steerID2 = 5;
+    public static final int absEncoder2 = 6;
+
+    public static final double turning2P = 0.0;
+    public static final double turning2I = 0.0;
+    public static final double turning2D = 0.0;
+
+    public static final int drivelID3 = 7;
+    public static final int steerID4 = 8;
+    public static final int absEncoder3 = 9;
+
+    public static final double turning3P = 0.0;
+    public static final double turning3I = 0.0;
+    public static final double turning3D = 0.0;
+
+    public static final int driveID5 = 10;
+    public static final int steerID5 = 11;
+    public static final int absEncoder4 = 12;
+
+    public static final double turning4P = 0.0;
+    public static final double turning4I = 0.0;
+    public static final double turning4D = 0.0;
+
+}
