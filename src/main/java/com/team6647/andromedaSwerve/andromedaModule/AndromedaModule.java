9233a78dcb2ff51dd70deca9e0b80f44844ea6a7
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-11 21:56:18 -0600

    Added constructor

--- src/main/java/com/team6647/andromedaSwerve/andromedaModule/AndromedaModule.java
+++ src/main/java/com/team6647/andromedaSwerve/andromedaModule/AndromedaModule.java
@@ -3,6 +3,24 @@
  */
 package com.team6647.andromedaSwerve.andromedaModule;
 
-public class AndromedaModule {
+import com.andromedalib.motorControllers.SuperTalonFX;
+import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.ctre.phoenix.sensors.CANCoder;
 
+import edu.wpi.first.math.controller.PIDController;
+
+public class AndromedaModule {
+    private SuperTalonFX driveMotor;
+    private SuperTalonFX steeringMotor;
+    private CANCoder steeringEncoder;
+
+    private PIDController turningPID;
+
+    public AndromedaModule(int driveID, int steeringID, int absEncoderID){
+        this.driveMotor = new SuperTalonFX(driveID, GlobalIdleMode.brake, false);
+        this.steeringMotor = new SuperTalonFX(steeringID, GlobalIdleMode.brake, false);
+        this.steeringEncoder = new CANCoder(absEncoderID);
+
+        
+    }
 }
