7e9c0bbe120f20e21641ea48e64807389ea81459
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-12 18:25:00 -0600

    Removed andromedaConfig

--- src/main/java/com/team6647/andromedaSwerve/utils/AndromedaConfig.java
+++
@@ -1,9 +0,0 @@
-/**
- * Written by Juan Pablo Gutiérrez
- */
-package com.team6647.andromedaSwerve.utils;
-
-/* Manages configuration variables for the modules */
-public class AndromedaConfig {
-    
-}
