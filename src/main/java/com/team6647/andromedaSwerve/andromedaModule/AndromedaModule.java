4e83cea9b7b980328862cff444eb4a02545ba366
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-12 18:25:55 -0600

    Changed AndromedaModule to include constants

--- src/main/java/com/team6647/andromedaSwerve/andromedaModule/AndromedaModule.java
+++ src/main/java/com/team6647/andromedaSwerve/andromedaModule/AndromedaModule.java
@@ -6,19 +6,17 @@ package com.team6647.andromedaSwerve.andromedaModule;
 import com.andromedalib.motorControllers.SuperTalonFX;
 import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
 import com.ctre.phoenix.sensors.CANCoder;
-
-import edu.wpi.first.math.controller.PIDController;
+import com.team6647.andromedaSwerve.utils.AndromedaModuleConstants;
 
 public class AndromedaModule {
     private SuperTalonFX driveMotor;
     private SuperTalonFX steeringMotor;
     private CANCoder steeringEncoder;
+    private AndromedaModuleConstants constants;
 
-    private PIDController turningPID;
-
-    public AndromedaModule(int driveID, int steeringID, int absEncoderID){
-        this.driveMotor = new SuperTalonFX(driveID, GlobalIdleMode.brake, false);
-        this.steeringMotor = new SuperTalonFX(steeringID, GlobalIdleMode.brake, false);
-        this.steeringEncoder = new CANCoder(absEncoderID);
+    public AndromedaModule(AndromedaModuleConstants constants){
+        this.driveMotor = new SuperTalonFX(constants.driveMotorID, GlobalIdleMode.brake, false);
+        this.steeringMotor = new SuperTalonFX(constants.steeringMotorID, GlobalIdleMode.brake, false);
+        this.steeringEncoder = new CANCoder(constants.absCanCoderID);
     }
 }
