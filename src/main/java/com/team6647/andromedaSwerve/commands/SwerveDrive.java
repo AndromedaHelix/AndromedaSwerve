671a0a0a638c50435decb91069c1be169ffd4b36
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-14 17:50:48 -0600

    Created swerveDrive command

---
+++ src/main/java/com/team6647/andromedaSwerve/commands/SwerveDrive.java
@@ -0,0 +1,50 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+package com.team6647.andromedaSwerve.commands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import com.andromedalib.math.Functions;
+import com.team6647.andromedaSwerve.systems.AndromedaSwerve;
+import com.team6647.util.Constants.SwerveConstants;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SwerveDrive extends CommandBase {
+
+  AndromedaSwerve swerve;
+  DoubleSupplier translationY;
+  DoubleSupplier translationX;
+  DoubleSupplier rotation;
+  BooleanSupplier fieldOriented;
+
+  /** Creates a new SwerveDrive. */
+  public SwerveDrive(AndromedaSwerve andromedaSwerve, DoubleSupplier translationX, DoubleSupplier translationY,
+      DoubleSupplier rotation, BooleanSupplier fieldOrientedControl) {
+    this.swerve = andromedaSwerve;
+    this.translationY = translationY;
+    this.translationX = translationX;
+    this.rotation = rotation;
+    this.fieldOriented = fieldOrientedControl;
+
+    addRequirements(swerve);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double translationYVal = Functions.handleDeadband(translationY.getAsDouble(), SwerveConstants.deadband);
+    double translationXVal = Functions.handleDeadband(translationX.getAsDouble(), SwerveConstants.deadband);
+    double rotationVal = Functions.handleDeadband(rotation.getAsDouble(), SwerveConstants.deadband);
+
+    swerve.drive(
+        new Translation2d(translationYVal, translationXVal).times(SwerveConstants.maxSpeed),
+        rotationVal * SwerveConstants.maxAngularVelocity,
+        fieldOriented.getAsBoolean(),
+        true);
+  }
+
+}
