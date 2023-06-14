15e14047ecb2b2349c0c08913f7019ad751a53f5
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-13 21:10:23 -0600

    Worked on module methods

--- src/main/java/com/team6647/andromedaSwerve/andromedaModule/AndromedaModule.java
+++ src/main/java/com/team6647/andromedaSwerve/andromedaModule/AndromedaModule.java
@@ -3,24 +3,47 @@
  */
 package com.team6647.andromedaSwerve.andromedaModule;
 
+import com.andromedalib.math.Conversions;
 import com.andromedalib.motorControllers.SuperTalonFX;
 import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
-import com.ctre.phoenix.sensors.CANCoder;
+import com.andromedalib.sensors.SuperCANCoder;
 import com.team6647.andromedaSwerve.utils.AndromedaModuleConstants;
+import com.team6647.util.Constants.SwerveConstants;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class AndromedaModule {
     private int moduleNumber;
 
     private SuperTalonFX driveMotor;
     private SuperTalonFX steeringMotor;
-    private CANCoder steeringEncoder;
+    private SuperCANCoder steeringEncoder;
     private AndromedaModuleConstants constants;
+
+    private Rotation2d angleOffset;
 
     public AndromedaModule(int moduleNumber, AndromedaModuleConstants constants) {
         this.moduleNumber = moduleNumber;
         
-        this.driveMotor = new SuperTalonFX(constants.driveMotorID, GlobalIdleMode.brake, false);
-        this.steeringMotor = new SuperTalonFX(constants.steeringMotorID, GlobalIdleMode.Coast, false);
-        this.steeringEncoder = new CANCoder(constants.absCanCoderID);
+        this.driveMotor = new SuperTalonFX(constants.driveMotorID, GlobalIdleMode.brake, false, SwerveConstants.andromedaProfile.driveMotorConfiguration);
+        this.steeringMotor = new SuperTalonFX(constants.steeringMotorID, GlobalIdleMode.Coast, false, SwerveConstants.andromedaProfile.turningMotorConfiguration);
+        this.steeringEncoder = new SuperCANCoder(constants.absCanCoderID, SwerveConstants.andromedaProfile.cancoderConfig);
+
+        this.angleOffset = constants.angleOffset;
+
+        resetAbsolutePosition();
     }
+
+    public int getModuleNumber(){
+        return moduleNumber;
+    }
+
+    public void resetAbsolutePosition(){
+        steeringMotor.setSelectedSensorPosition(0);
+        double encoderPosition = Conversions.degreesToFalcon(steeringEncoder.getDegrees() - angleOffset.getDegrees(), SwerveConstants.andromedaProfile.steeringGearRatio);
+        steeringMotor.setSelectedSensorPosition(encoderPosition);
+    }
+
+  
 }
